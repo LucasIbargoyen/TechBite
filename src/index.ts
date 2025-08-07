0d6f7d6bba7f3c86e367c816db23e5a39782c011
LucasIbargoyen <62619004+LucasIbargoyen@users.noreply.github.com>
2025-08-07 20:36:45 -0300

    Add files via upload

---
+++ src/index.ts
@@ -0,0 +1,61 @@
+
+import { Pedido, Produto  } from "./pedido"
+
+const cafe =  new Produto("Café", 5);
+const refrigerante: Produto = new Produto("Refrigerante", 4.5)
+const paoovo = new Produto("Pão com Ovo", 8.75)
+const xmonstro = new Produto("X-Monstro", 12.5)
+
+// PEDIDO 1
+
+const pedido1: Pedido = new Pedido("pedido1")
+pedido1.adicionarProduto(xmonstro)
+pedido1.adicionarProduto(refrigerante)
+pedido1.listar()
+pedido1.calcularTotal()
+console.log("\n")
+pedido1.alterarDesconto(0.1)
+pedido1.listar()
+pedido1.calcularTotal()
+
+
+
+
+const pedido2: Pedido = new Pedido("pedido2")
+pedido2.alterarDesconto(0.1)
+pedido2.adicionarProduto(cafe)
+pedido2.adicionarProduto(paoovo)
+pedido2.listar()
+pedido2.calcularTotal()
+
+// inicia e cria o pedido
+// const pedido1: Produto[] = [];
+// console.log("iniciando o pedido1...");
+// // adiciona produtos ao pedido
+// adicionarProduto(pedido1, refrigerante);
+// adicionarProduto(pedido1, paoovo);
+// adicionarProduto(pedido1, paoovo);
+// // aplicar desconto
+// const desconto1 = 0.1; // 10% de desconto
+// // listar o pedido
+// listarPedido(pedido1, desconto1);
+// // calcular total do pedido
+// calcularTotal({ pedido: pedido1, desconto: desconto1 });
+
+
+// inicia e cria o pedido
+// const pedido2: Produto[] = [];
+// console.log("iniciando o pedido2...");
+// // adiciona produtos ao pedido
+// adicionarProduto(pedido2, refrigerante);
+// adicionarProduto(pedido2, paoovo);
+// adicionarProduto(pedido2, paoovo);
+// // aplicar desconto
+// const desconto2 = 0.2; // 20% de desconto
+// // listar o pedido
+// listarPedido(pedido2, desconto2);
+// // calcular total do pedido
+// calcularTotal({ pedido: pedido2, desconto: desconto2 });
+
+
+
---
+++ src/pagamento.ts
@@ -0,0 +1,24 @@
+
+
+export interface Pagamento{
+    pagar(): void;
+}
+
+export class PagamentoCartao implements Pagamento {
+    pagar(): void {
+        console.log("Pagamento realizado com cartão.");
+    }
+}
+
+export class PagamentoDinheiro implements Pagamento {
+    pagar(): void {
+        console.log("Pagamento realizado em dinheiro.");
+    }
+}
+
+export class PagamentoPix implements Pagamento {
+    pagar(): void {
+        console.log("Pagamento realizado via Pix.");
+    }
+}   
+
